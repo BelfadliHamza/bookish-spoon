be6a10d828d8967d7b9f3dd9e1d22fe6da7508f3
BelfadliHamza <hamzabelfadli99@gmail.com>
2026-03-02 13:12:21 +0100

    "add-navbar"

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+
+const Navbar = () => {
+    return (
+        <header className="w-full fixed z-50 bg-('--bg-primary')">
+            <div className="wrapper navbar-height py-4 flex justify-between items-center">
+                <Link href="/" className="flex gap-0.5 items-center">
+                    <Image src="/assets/logo.png" alt="Bookfied" width={42} height={26} />
+                    <span className="logo-text">Bookified</span>
+                </Link>
+
+            </div>
+        </header>
+    )
+}
+export default Navbar
